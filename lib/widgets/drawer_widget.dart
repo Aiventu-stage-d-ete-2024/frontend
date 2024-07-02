4fa4806d48264849bcf5cb831c2559bb1a9730e5
MarsaouiMariem <marsaoui81@gmail.com>
2024-07-02 14:32:35 +0100

    update final design + tables

--- lib/widgets/drawer_widget.dart
+++ lib/widgets/drawer_widget.dart
@@ -6,6 +6,7 @@ class DrawerWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
+      backgroundColor: Colors.white,
       child: ListView(
         padding: EdgeInsets.zero,
         children: [
