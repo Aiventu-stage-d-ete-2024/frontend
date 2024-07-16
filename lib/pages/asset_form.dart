e1d55dad60e9b9e4ef2344c536b69ccbc98b6e79
MarsaouiMariem <marsaoui81@gmail.com>
2024-07-16 08:23:58 +0100

    asset creation done

--- lib/pages/asset_form.dart
+++ lib/pages/asset_form.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import '../baseURL.dart';
 import '../pages/asset_page.dart';
 import '../widgets/main_app_bar.dart';
 import '../widgets/drawer_widget.dart';
-import '../widgets/asset_app_bar_body.dart'; // Import here
+import '../widgets/asset_app_bar_body.dart';
 
 class AssetForm extends StatefulWidget {
   const AssetForm({super.key});
@@ -12,12 +15,80 @@ class AssetForm extends StatefulWidget {
 }
 
 class _AssetFormState extends State<AssetForm> {
-  final TextEditingController _filterController = TextEditingController();
+  final TextEditingController _assetIdController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _parentController = TextEditingController();
+  final TextEditingController _numberOfChildrenController = TextEditingController();
+  final TextEditingController _assetTypeController = TextEditingController();
+  final TextEditingController _manufacturerController = TextEditingController();
+  final TextEditingController _modelController = TextEditingController();
+  final TextEditingController _customerAccountController = TextEditingController();
+  final TextEditingController _criticalityController = TextEditingController();
+  final TextEditingController _functionalLocationController = TextEditingController();
+  final TextEditingController _currentLifecycleStateController = TextEditingController();
 
   @override
   void dispose() {
-    _filterController.dispose();
+    _assetIdController.dispose();
+    _nameController.dispose();
+    _parentController.dispose();
+    _numberOfChildrenController.dispose();
+    _assetTypeController.dispose();
+    _manufacturerController.dispose();
+    _modelController.dispose();
+    _customerAccountController.dispose();
+    _criticalityController.dispose();
+    _functionalLocationController.dispose();
+    _currentLifecycleStateController.dispose();
     super.dispose();
+  }
+
+  Future<void> _createAsset() async {
+    final url = Uri.parse('${baseUrl}assets');
+    final response = await http.post(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode({
+        'AssetID': _assetIdController.text,
+        'Name': _nameController.text,
+        'Parent': _parentController.text,
+        'NumberOfChildren': _numberOfChildrenController.text,
+        'AssetType': _assetTypeController.text,
+        'Manufacturer': _manufacturerController.text,
+        'Model': _modelController.text,
+        'CustomerAccount': _customerAccountController.text,
+        'Criticality': _criticalityController.text,
+        'FunctionalLocation': _functionalLocationController.text,
+        'CurrentLifecycleState': _currentLifecycleStateController.text,
+      }),
+    );
+
+    if (response.statusCode == 201) {
+      Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(builder: (context) => const AssetPage()),
+        (Route<dynamic> route) => false,
+      );
+    } else {
+      // Handle the error
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Error'),
+            content: Text('Failed to create asset'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    }
   }
 
   @override
@@ -35,25 +106,25 @@ class _AssetFormState extends State<AssetForm> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  /*Center(
-                    child: Text(
-                      'Asset Form',
-                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                    ),
-                  ),*/
                   const SizedBox(height: 20.0),
-                  const AssetFormFields(),
+                  AssetFormFields(
+                    assetIdController: _assetIdController,
+                    nameController: _nameController,
+                    parentController:_parentController,
+                    numberOfChildrenController: _numberOfChildrenController,
+                    assetTypeController: _assetTypeController,
+                    manufacturerController: _manufacturerController,
+                    modelController: _modelController,
+                    customerAccountController: _customerAccountController,
+                    criticalityController: _criticalityController,
+                    functionalLocationController: _functionalLocationController,
+                    currentLifecycleStateController: _currentLifecycleStateController,
+                  ),
                   const SizedBox(height: 20.0),
                   Align(
                     alignment: Alignment.center,
                     child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.pushAndRemoveUntil(
-                          context,
-                          MaterialPageRoute(builder: (context) => const AssetPage()),
-                          (Route<dynamic> route) => false,
-                        );
-                      },
+                      onPressed: _createAsset,
                       style: ElevatedButton.styleFrom(
                         padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 12),
                         textStyle: const TextStyle(fontSize: 18),
@@ -72,35 +143,95 @@ class _AssetFormState extends State<AssetForm> {
 }
 
 class AssetFormFields extends StatelessWidget {
-  const AssetFormFields({super.key});
+  final TextEditingController assetIdController;
+  final TextEditingController nameController;
+  final TextEditingController parentController;
+  final TextEditingController numberOfChildrenController;
+  final TextEditingController assetTypeController;
+  final TextEditingController manufacturerController;
+  final TextEditingController modelController;
+  final TextEditingController customerAccountController;
+  final TextEditingController criticalityController;
+  final TextEditingController functionalLocationController;
+  final TextEditingController currentLifecycleStateController;
+
+  const AssetFormFields({
+    super.key,
+    required this.assetIdController,
+    required this.nameController,
+    required this.parentController,
+    required this.numberOfChildrenController,
+    required this.assetTypeController,
+    required this.manufacturerController,
+    required this.modelController,
+    required this.customerAccountController,
+    required this.criticalityController,
+    required this.functionalLocationController,
+    required this.currentLifecycleStateController,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: SizedBox(
         width: MediaQuery.of(context).size.width * 0.9,
-        child: const Column(
+        child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            CustomTextFormField(labelText: 'Asset ID'),
-            SizedBox(height: 20.0),
-            CustomTextFormField(labelText: 'Name'),
-            SizedBox(height: 20.0),
-            CustomTextFormField(labelText: 'Number Of Children'),
-            SizedBox(height: 20.0),
-            CustomTextFormField(labelText: 'Asset Type'),
-            SizedBox(height: 20.0),
-            CustomTextFormField(labelText: 'Manufacturer'),
-            SizedBox(height: 20.0),
-            CustomTextFormField(labelText: 'Model'),
-            SizedBox(height: 20.0),
-            CustomTextFormField(labelText: 'Customer Account'),
-            SizedBox(height: 20.0),
-            CustomTextFormField(labelText: 'Criticality'),
-            SizedBox(height: 20.0),
-            CustomTextFormField(labelText: 'Functional Location'),
-            SizedBox(height: 20.0),
-            CustomTextFormField(labelText: 'Current Lifecycle State'),
+            CustomTextFormField(
+              labelText: 'Asset ID',
+              controller: assetIdController,
+            ),
+            const SizedBox(height: 20.0),
+            CustomTextFormField(
+              labelText: 'Name',
+              controller: nameController,
+            ),
+            const SizedBox(height: 20.0),
+            CustomTextFormField(
+              labelText: 'Parent',
+              controller: parentController,
+            ),
+            const SizedBox(height: 20.0),
+            CustomTextFormField(
+              labelText: 'Number Of Children',
+              controller: numberOfChildrenController,
+            ),
+            const SizedBox(height: 20.0),
+            CustomTextFormField(
+              labelText: 'Asset Type',
+              controller: assetTypeController,
+            ),
+            const SizedBox(height: 20.0),
+            CustomTextFormField(
+              labelText: 'Manufacturer',
+              controller: manufacturerController,
+            ),
+            const SizedBox(height: 20.0),
+            CustomTextFormField(
+              labelText: 'Model',
+              controller: modelController,
+            ),
+            const SizedBox(height: 20.0),
+            CustomTextFormField(
+              labelText: 'Customer Account',
+              controller: customerAccountController,
+            ),
+            const SizedBox(height: 20.0),
+            CustomTextFormField(
+              labelText: 'Criticality',
+              controller: criticalityController,
+            ),
+            const SizedBox(height: 20.0),
+            CustomTextFormField(
+              labelText: 'Functional Location',
+              controller: functionalLocationController,
+            ),
+            const SizedBox(height: 20.0),
+            CustomTextFormField(
+              labelText: 'Current Lifecycle State',
+              controller: currentLifecycleStateController,
+            ),
           ],
         ),
       ),
@@ -110,12 +241,18 @@ class AssetFormFields extends StatelessWidget {
 
 class CustomTextFormField extends StatelessWidget {
   final String labelText;
+  final TextEditingController controller;
 
-  const CustomTextFormField({super.key, required this.labelText});
+  const CustomTextFormField({
+    super.key,
+    required this.labelText,
+    required this.controller,
+  });
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      controller: controller,
       decoration: InputDecoration(
         labelText: labelText,
         labelStyle: const TextStyle(
@@ -125,7 +262,7 @@ class CustomTextFormField extends StatelessWidget {
         floatingLabelStyle: const TextStyle(
           fontSize: 18,
           fontWeight: FontWeight.bold,
-          color: Color(0xFF3665DB)
+          color: Color(0xFF3665DB),
         ),
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(10.0),
